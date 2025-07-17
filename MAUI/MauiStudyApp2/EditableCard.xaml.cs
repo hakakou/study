6c0eeb0c04167af599bf9f52e90e5ce82a636314
Harry Kakoulidis <harry@uptiv.com>
2025-07-17 11:49:03 +0300

    Extract MAUIStudyApp2 from MauiApp1 and rework Page4 structure

---
+++ MAUI/MauiStudyApp2/EditableCard.xaml.cs
@@ -0,0 +1,39 @@
+namespace MauiStudyApp2;
+
+public partial class EditableCard : ContentView
+{
+    public EditableCard()
+    {
+        InitializeComponent();
+        BindingContext = this;
+    }
+
+    bool isEditing;
+
+    private void OnEditButtonClicked(object sender, EventArgs e)
+    {
+        isEditing = !isEditing;
+        if (isEditing)
+        {
+            editor.IsReadOnly = false;
+            editor.Focus();
+            editor.CursorPosition = editor.Text == null ? 0 : editor.Text.Length;
+            editButton.Text = "Save";
+        }
+        else
+        {
+            editor.IsReadOnly = true;
+            editButton.Focus();
+            editButton.Text = "Edit";
+        }
+    }
+
+    public string Text
+    {
+        get { return (string)GetValue(TextProperty); }
+        set { SetValue(TextProperty, value); }
+    }
+
+    public static readonly BindableProperty TextProperty =
+        BindableProperty.Create("Text", typeof(string), typeof(EditableCard));
+}
--- MAUI/MauiStudyApp2/EntrySelection.cs
+++ MAUI/MauiStudyApp2/EntrySelection.cs
@@ -1,95 +1,159 @@
-﻿using System;
+﻿using Microsoft.Maui.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MauiStudyApp2
+namespace MauiStudyApp2;
+
+public static class EntrySelection
 {
-    public static class EntrySelection
+    public static readonly BindableProperty SelectAllOnFocusProperty =
+        BindableProperty.CreateAttached(
+            "SelectAllOnFocus",
+            typeof(bool),
+            typeof(EntrySelection),
+            false,
+            propertyChanged: OnSelectAllOnFocusChanged);
+
+    public static bool GetSelectAllOnFocus(BindableObject view) =>
+        (bool)view.GetValue(SelectAllOnFocusProperty);
+
+    public static void SetSelectAllOnFocus(BindableObject view, bool value) =>
+        view.SetValue(SelectAllOnFocusProperty, value);
+
+    public static void OnSelectAllOnFocusChanged(
+        BindableObject obj, object oldValue, object newValue)
     {
-        public static readonly BindableProperty SelectAllOnFocusProperty =
-            BindableProperty.CreateAttached(
-                "SelectAllOnFocus",
-                typeof(bool),
-                typeof(EntrySelection),
-                false,
-                propertyChanged: OnSelectAllOnFocusChanged);
-
-        public static bool GetSelectAllOnFocus(BindableObject view) =>
-            (bool)view.GetValue(SelectAllOnFocusProperty);
-
-        public static void SetSelectAllOnFocus(BindableObject view, bool value) =>
-            view.SetValue(SelectAllOnFocusProperty, value);
-
-        public static void OnSelectAllOnFocusChanged(
-            BindableObject obj, object oldValue, object newValue)
+        if (obj is Entry entry)
         {
-            if (obj is Entry entry)
+            if ((bool)newValue)
             {
-                if ((bool)newValue)
-                {
-                    entry.Focused += OnEntryFocused;
-                }
-                else
-                {
-                    entry.Focused -= OnEntryFocused;
-                }
+                entry.Focused += OnEntryFocused;
+            }
+            else
+            {
+                entry.Focused -= OnEntryFocused;
             }
         }
+    }
 
-        private static async void OnEntryFocused(object? sender, FocusEventArgs e)
+    private static async void OnEntryFocused(object? sender, FocusEventArgs e)
+    {
+        if (sender is Entry entry && e.IsFocused)
         {
-            if (sender is Entry entry && e.IsFocused)
+            // Delay to ensure the focus event is fully processed
+            await Task.Delay(100);
+            if (entry.IsFocused && !string.IsNullOrEmpty(entry.Text))
             {
-                // Delay to ensure the focus event is fully processed
-                await Task.Delay(100);
-                if (entry.IsFocused && !string.IsNullOrEmpty(entry.Text))
-                {
-                    entry.CursorPosition = 0;
-                    entry.SelectionLength = entry.Text.Length;
-                }
+                entry.CursorPosition = 0;
+                entry.SelectionLength = entry.Text.Length;
             }
         }
     }
 }
 
-public static class HtmlText
+public class DoubleTapToZoomBehavior : Behavior<Image>
 {
-    public static readonly BindableProperty IsHtmlProperty =
-        BindableProperty.CreateAttached("IsHtml", typeof(bool),
-            typeof(HtmlText), false, propertyChanged: OnHtmlPropertyChanged);
+    public static readonly BindableProperty ScaleFactorProperty =
+            BindableProperty.Create(
+                nameof(ScaleFactor),
+                typeof(double),
+                typeof(DoubleTapToZoomBehavior),
+                2.0);
 
-    public static bool GetIsHtml(BindableObject view)
+    // Making the property bindable will allow you not only to set it when using
+    // DoubleTapToZoomBehavior, but also to bind it to a view model or another control.
+
+    public double ScaleFactor
     {
-        return (bool)view.GetValue(IsHtmlProperty);
+        get => (double)GetValue(ScaleFactorProperty);
+        set => SetValue(ScaleFactorProperty, value);
     }
-
-    public static void SetIsHtml(BindableObject view, bool value)
+    protected override void OnAttachedTo(Image bindable)
     {
-        view.SetValue(IsHtmlProperty, value);
-    }
-
-    private static void OnHtmlPropertyChanged(BindableObject bindable, object oldValue, object newValue)
-    {
-        if (bindable is Label label)
+        base.OnAttachedTo(bindable);
+        image = bindable;
+        tapGestureRecognizer = new TapGestureRecognizer()
         {
+            NumberOfTapsRequired = 2
+        };
+
+        tapGestureRecognizer.Tapped += OnImageDoubleTap;
+        image.GestureRecognizers.Add(tapGestureRecognizer);
+    }
+
+    protected override void OnDetachingFrom(Image bindable)
+    {
+        base.OnDetachingFrom(bindable);
+        if (tapGestureRecognizer != null)
+        {
+            tapGestureRecognizer.Tapped -= OnImageDoubleTap;
+            image.GestureRecognizers.Remove(tapGestureRecognizer);
+        }
+        image = null;
+        // We also set the image field to null in order to not
+        // hold a reference to the parent Image object.
+    }
+
+    private void OnImageDoubleTap(object sender, TappedEventArgs e)
+    {
+        Point? tappedPoint = e.GetPosition(image);
+        if (isZoomed)
+        {
+            image.ScaleTo(1);
+            image.TranslateTo(0, 0);
+            isZoomed = false;
+        }
+        else
+        {
+            double translateFactor = ScaleFactor - 1;
+            double translateX = (image.Width / 2 - tappedPoint.Value.X) * translateFactor;
+            double translateY = (image.Height / 2 - tappedPoint.Value.Y) * translateFactor;
+            image.TranslateTo(translateX, translateY);
+            image.ScaleTo(ScaleFactor);
+            isZoomed = true;
         }
     }
-}
 
-public class FancyFrame : Frame
-{
-    public static readonly BindableProperty CornerRadiusProperty =
-        BindableProperty.Create(
-            nameof(CornerRadius),
-            typeof(double),
-            typeof(FancyFrame),
-            0.0);
+    private bool isZoomed;
+    private Image image;
+    private TapGestureRecognizer tapGestureRecognizer;
 
-    public double CornerRadius
+    // Attached Property to easily add/remove the behavior in XAML  
+
+    public static readonly BindableProperty AttachBehaviorProperty =
+    BindableProperty.CreateAttached("AttachBehavior", typeof(bool),
+        typeof(DoubleTapToZoomBehavior), false,
+        propertyChanged: OnAttachBehaviorChanged);
+
+    public static bool GetAttachBehavior(BindableObject view) =>
+        (bool)view.GetValue(AttachBehaviorProperty);
+
+    public static void SetAttachBehavior(BindableObject view, bool value) =>
+        view.SetValue(AttachBehaviorProperty, value);
+
+    static void OnAttachBehaviorChanged(BindableObject view, object oldValue, object newValue)
     {
-        get => (double)GetValue(CornerRadiusProperty);
-        set => SetValue(CornerRadiusProperty, value);
+        var img = view as Image;
+        if (img == null)
+            return;
+
+        bool shouldAttach = (bool)newValue;
+
+        if (shouldAttach)
+        {
+            img.Behaviors.Add(new DoubleTapToZoomBehavior());
+        }
+        else
+        {
+            var toRemove = img.Behaviors
+                .FirstOrDefault(b => b is DoubleTapToZoomBehavior);
+
+            if (toRemove != null)
+                img.Behaviors.Remove(toRemove);
+        }
     }
 }
